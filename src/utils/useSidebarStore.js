431485da45b4540a258b8c4da530e5e5b1ae44e9
Guffy-san <vinaybasargekar13@gmail.com>
2024-08-04 16:42:41 +0530

    init

--- src/utils/useSidebarStore.js
+++ src/utils/useSidebarStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 const useSidebarStore = create((set) => ({
-	sidebarOpen: false,
+	sidebarOpen: true,
 	setSidebarOpen: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
 }));
 
